3b07550891701ace5b2602389a48a715d78f59a4
jiangsu <1044699026@qq.com>
2020-10-29 10:20:01 +0800

    完成了首页功能

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,25 @@
+//开发环境地址
+var baseUrl = "http://ajax.frontend.itheima.net"
+    //测试环境地址
+    // var baseUrl = "http://ajax.frontend.itheima.net"
+    //生产环境地址
+    // var baseUrl = "http://ajax.frontend.itheima.net"
+
+
+
+
+$.ajaxPrefilter(function(options) {
+    options.url = baseUrl + options.url
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem("token") || ""
+        }
+    }
+    options.complete=function(res){
+     var obj=res.responseJSON;
+     if (obj.status==1&&obj.message=="身份认证失败！"){
+         localStorage.removeItem('token')
+         location.href="/login.html"
+     }
+    }
+})
